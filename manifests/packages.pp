c76595ccec0f0a9857ec346cd5aa280471f3ffe0
akep0280 <akepley@gmail.com>
2015-06-23 12:27:08 -0500

    Initial php puppet

---
+++ manifests/packages.pp
@@ -0,0 +1,27 @@
+class php::package {
+
+#install REMI package repository
+file {'/etc/yum.repos.d/remi.repo':
+  ensure => 'file',
+  owner  => 'root'
+  group  => 'root',
+  mode   => '0644',
+  source => $::operatingsystem ? {
+    'RedHat' => 'puppet:///modules/php/remi.rh.repo',
+    'Scientific' => 'puppet:///modules/php/remi.sl.repo',
+    'default'  => fail("Unsupported OS: ${::operatingsystem}"),
+  }
+
+#install NASBA package repository
+file {'/etc/yum.repos.d/my.repo'}
+  ensure => 'file',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
+  source => $::operatingsystem ? {
+    'RedHat' => 'puppet:///modules/php/my.rh.repo',
+    'Scientific' => 'puppet:///modules/php/my.sl.repo',
+    'default'  => fail("Unsupported OS: ${::operatingsystem}"),
+  }
+
+  
