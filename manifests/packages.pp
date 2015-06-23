916c1e645c5d89b09316aa75f67ad7ae7d016a7a
akep0280 <akepley@gmail.com>
2015-06-23 13:41:23 -0500

    php fixed code 3

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -21,7 +21,7 @@ file {'/etc/yum.repos.d/my.repo':
   source => $::operatingsystem ? {
     'RedHat' => 'puppet:///modules/php/my.rh.repo',
     'Scientific' => 'puppet:///modules/php/my.sl.repo',
-    'default'  => 'puppet:///modules/php/my.rh.repo',
+    'CentOS'  => 'puppet:///modules/php/my.rh.repo',
     }
   }
 }
