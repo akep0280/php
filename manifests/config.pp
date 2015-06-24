cca701e8e65f9c73fe61613bacc1490202157379
akep0280 <akepley@gmail.com>
2015-06-24 09:03:27 -0500

    php.ini addition

---
+++ manifests/config.pp
@@ -0,0 +1,14 @@
+class php::config {
+
+
+
+file {'/etc/php.ini':
+  ensure => 'file',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
+  source => puppet:///modules/php/php.ini,
+  }
+
+
+}
