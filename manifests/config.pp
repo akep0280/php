ae30ec060e3cab4ee772f6d8c118f21a454c70a0
akep0280 <akepley@gmail.com>
2015-08-05 13:01:40 -0500

    Initial commit

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,15 +1,37 @@
 class php::config {
+  include stdlib
 
 
 
-file {'/etc/php.ini':
-  ensure   => 'file',
-  owner    => 'root',
-  group    => 'root',
-  mode     => '0644',
-  source   => 'puppet:///modules/php/php.ini',
-  require  => Package['php'],
+#file {'/etc/php.ini':
+  #ensure   => 'file',
+  #owner    => 'root',
+  #group    => 'root',
+  #mode     => '0644',
+  #source   => 'puppet:///modules/php/php.ini',
+  #require  => Package['php'],
+  #}
+
+  file_line { 'expose_off':
+    path  => '/etc/php.ini',
+    line  => 'expose_php = Off',
+    match => '^expose_php = On',
   }
-
+  file_line { 'session_1':
+    path  => '/etc/php.ini',
+    line  => 'session.name = NASBA',
+    }
+  file_line { 'session_2':
+    path  => '/etc/php.ini',
+    line  => 'session.cookie_secure = 1',
+    }
+  file_line { 'session_3':
+    path  => '/etc/php.ini',
+    line  => 'session.hash_function = 1',
+    }
+  file_line { 'session_4':
+    path  => '/etc/php.ini',
+    line  => 'session.cookie_httponly = 1',
+    }
 
 }
