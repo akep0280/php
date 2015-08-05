907d698325b1ad9489c844951feaa21ca7e0da39
akep0280 <akepley@gmail.com>
2015-08-05 13:23:06 -0500

    php change

--- manifests/config.pp
+++ manifests/config.pp
@@ -16,22 +16,27 @@ class php::config {
     path  => '/etc/php.ini',
     line  => 'expose_php = Off',
     match => '^expose_php = On',
+    notify => Service["httpd"],
   }
   file_line { 'session_1':
     path  => '/etc/php.ini',
     line  => 'session.name = NASBA',
+    notify => Service["httpd"],
     }
   file_line { 'session_2':
     path  => '/etc/php.ini',
     line  => 'session.cookie_secure = 1',
+    notify => Service["httpd"],
     }
   file_line { 'session_3':
     path  => '/etc/php.ini',
     line  => 'session.hash_function = 1',
+    notify => Service["httpd"],
     }
   file_line { 'session_4':
     path  => '/etc/php.ini',
     line  => 'session.cookie_httponly = 1',
+    notify => Service["httpd"],
     }
 
 }
